01585c49f5e804a6249cb94352e3f73ddad147b5
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2021-08-07 21:14:59 -0300

    env: Configurando Swagger para API

--- ComicShop.WebApi/Startup.cs
+++ ComicShop.WebApi/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace ComicShop.WebApi
 {
@@ -24,6 +28,27 @@ namespace ComicShop.WebApi
 
             services.AddDbContext<ComicShopCommonDbContext>(options =>
                    options.UseSqlServer(Configuration.GetConnectionString("ComicShopContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ComicShop WebApi",
+                    Description = "API destinada para gerenciar uma loja de quadrinhos",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Arnaldo Madeira",
+                        Email = "arnaldomadeira145@gmail.com"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +58,17 @@ namespace ComicShop.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ComicShop Web API");
+            });
 
             app.UseHttpsRedirection();
 
@@ -59,6 +95,4 @@ namespace ComicShop.WebApi
             }
         }
     }
-
-
 }
