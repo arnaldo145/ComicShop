fc4976f5c06d3b831556f40e7497a43e955a0af8
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2022-03-18 18:18:01 -0300

    feat: Criando seed para inserir usuário admin padrão

---
+++ ComicShop.Infra.Data/Migrations/20220318203542_Seed-User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComicShop.Infra.Data.Migrations
+{
+    public partial class SeedUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SeedExtensions.SeedUser(migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM [dbo].[users]");
+        }
+    }
+}
---
+++ ComicShop.Infra.Data/SeedExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ComicShop.Infra.Helpers;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComicShop.Infra.Data
+{
+    public static class SeedExtensions
+    {
+        public static void SeedUser(this MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@$"INSERT INTO [dbo].[users] (Id, Type, Name, Email,Password) 
+                                    VALUES (NEWID(), 2,'UserAdmin','admin@admin.com','{EncryptionHelper.Encrypt("admin")}')");
+        }
+    }
+}
